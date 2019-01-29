4145251c093fdcd54a2a6bcacc1535782d216e35
陈玉 <yu3.chen@BKJK.COM>
2019-01-29 18:01:58 +0800

    [add] select testing

--- channel/channel.go
+++ channel/channel.go
@@ -23,11 +23,11 @@ func main()  {
 	}
 	fmt.Println("Done")
 
-	// select
+	// select, example 1:
 	start := time.Now()
 	c := make(chan interface{})
 	go func() {
-		time.Sleep(5*time.Second)
+		time.Sleep(1*time.Second)
 		close(c)
 	}()
 
@@ -38,5 +38,20 @@ func main()  {
 		fmt.Printf("Unblocked %v later.\n", time.Since(start))
 
 	}
+
+	// select, example 2:
+	c1 := make(chan interface{}); close(c1)
+	c2 := make(chan interface{}); close(c2)
+
+	var c1Count, c2Count int
+	for i:= 1000; i>=0; i--{
+		select {
+		case <-c1:
+			c1Count++
+		case <-c2:
+			c2Count++
+		}
+	}
+	fmt.Printf("c1Count: %d\nc2Clount: %d\n", c1Count, c2Count)
 
 }
